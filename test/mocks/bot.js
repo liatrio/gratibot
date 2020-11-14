334366b2eeb353e3dc55406e7a61fd40396a57c6
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-11-14 15:07:09 -0800

    Fix linting of mocks

--- test/mocks/bot.js
+++ test/mocks/bot.js
@@ -1,77 +1,69 @@
 const sinon = require("sinon");
 
 const mockBot = (controller) => ({
-    api: {
-        chat: {
-            postEphemeral: controller.message.bind(controller, "ephemeral"),
-            postMessage: (message) => {
-                if (message.thread_ts) {
-                    return controller.message.call(
-                        controller,
-                        "thread",
-                        message
-                    );
-                }
+  api: {
+    chat: {
+      postEphemeral: controller.message.bind(controller, "ephemeral"),
+      postMessage: (message) => {
+        if (message.thread_ts) {
+          return controller.message.call(controller, "thread", message);
+        }
 
-                return controller.message.call(controller, "main", message);
-            },
-            update: (message) => {
-                if (message.thread_ts) {
-                    return controller.update.call(
-                        controller,
-                        "thread",
-                        message
-                    );
-                }
+        return controller.message.call(controller, "main", message);
+      },
+      update: (message) => {
+        if (message.thread_ts) {
+          return controller.update.call(controller, "thread", message);
+        }
 
-                return controller.update.call(controller, "main", message);
-            },
-        },
-        reactions: {
-            add: function (reaction) {
-                return new Promise((resolve) => {
-                    this.reactions.push(reaction);
-
-                    resolve();
-                });
-            }.bind(controller),
-        },
-        users: {
-            lookupByEmail: sinon.stub(),
-            info: sinon.stub(),
-        },
-        channels: {
-            info: (/* args */) => ({
-                channel: {
-                    name: controller.info.channelName,
-                },
-            }),
-        },
-        files: {
-            upload: (file) => {
-                if (file.thread_ts) {
-                    return controller.file.call(controller, "thread", file);
-                }
-
-                return controller.file.call(controller, "main", file);
-            },
-        },
-        auth: {
-            test: sinon.stub(),
-        },
-        views: {},
+        return controller.update.call(controller, "main", message);
+      },
     },
-    reply: controller.reply.bind(controller, "channel"),
-    replyEphemeral: controller.reply.bind(controller, "ephemeral"),
-    replyInThread: controller.reply.bind(controller, "thread"),
-    replyAcknowledge: sinon.stub(),
-    say: controller.reply.bind(controller, "thread", []),
-    startPrivateConversation: sinon.stub(),
-    startConversationInThread: sinon.stub(),
-    startConversationInChannel: sinon.stub(),
+    reactions: {
+      add: function (reaction) {
+        return new Promise((resolve) => {
+          this.reactions.push(reaction);
 
-    beginDialog: sinon.stub(),
-    changeContext: sinon.stub(),
+          resolve();
+        });
+      }.bind(controller),
+    },
+    users: {
+      lookupByEmail: sinon.stub(),
+      info: sinon.stub(),
+    },
+    channels: {
+      info: (/* args */) => ({
+        channel: {
+          name: controller.info.channelName,
+        },
+      }),
+    },
+    files: {
+      upload: (file) => {
+        if (file.thread_ts) {
+          return controller.file.call(controller, "thread", file);
+        }
+
+        return controller.file.call(controller, "main", file);
+      },
+    },
+    auth: {
+      test: sinon.stub(),
+    },
+    views: {},
+  },
+  reply: controller.reply.bind(controller, "channel"),
+  replyEphemeral: controller.reply.bind(controller, "ephemeral"),
+  replyInThread: controller.reply.bind(controller, "thread"),
+  replyAcknowledge: sinon.stub(),
+  say: controller.reply.bind(controller, "thread", []),
+  startPrivateConversation: sinon.stub(),
+  startConversationInThread: sinon.stub(),
+  startConversationInChannel: sinon.stub(),
+
+  beginDialog: sinon.stub(),
+  changeContext: sinon.stub(),
 });
 
 module.exports = mockBot;
--- test/mocks/botkit.js
+++ test/mocks/botkit.js
@@ -1,25 +1,25 @@
 class MockBotkit {
-    constructor(controller) {
-        this.controller = controller;
-        this.plugins = controller.plugins;
-        this.BotkitConversation = BotkitConversation;
-    }
+  constructor(controller) {
+    this.controller = controller;
+    this.plugins = controller.plugins;
+    this.BotkitConversation = BotkitConversation;
+  }
 
-    spawn() {
-        return this.controller.bot;
-    }
+  spawn() {
+    return this.controller.bot;
+  }
 }
 
 class BotkitConversation {
-    constructor(dialogId, controller) {
-        this.dialogId = dialogId;
-        this.controller = controller;
-    }
+  constructor(dialogId, controller) {
+    this.dialogId = dialogId;
+    this.controller = controller;
+  }
 
-    async ask(response, callback /*, key */) {
-        this.controller.reply("Thread", [], response);
-        await callback(this.controller.userReply);
-    }
+  async ask(response, callback /*, key */) {
+    this.controller.reply("Thread", [], response);
+    await callback(this.controller.userReply);
+  }
 }
 
 module.exports = MockBotkit;

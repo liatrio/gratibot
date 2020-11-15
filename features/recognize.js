9f38f41af698b510e4893fa312201285a0eb3fc1
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-11-14 23:17:05 -0800

    Clean up recognize feature code

--- features/recognize.js
+++ features/recognize.js
@@ -7,7 +7,7 @@ const {
 } = require("../config");
 const recognition = require("../service/recognition");
 const balance = require("../service/balance");
-// const winston = require("../winston");
+const winston = require("../winston");
 
 const userRegex = /<@([a-zA-Z0-9]+)>/g;
 const tagRegex = /#(\S+)/g;
@@ -18,45 +18,34 @@ module.exports = function (controller) {
   controller.hears(
     recognizeEmoji,
     ["direct_message", "direct_mention", "mention", "message"],
-    async (bot, message) => {
-      const userInfo = await userDetails(bot, message, message.user);
-      if (!userInfo) {
-        // TODO Error handling
-        return;
-      }
-
-      const errors = await checkForRecognitionErrors(message, userInfo);
-      if (errors) {
-        await bot.replyEphemeral(
-          message,
-          [
-            `Sending ${recognizeEmoji} failed with the following error(s):`,
-            errors,
-          ].join("\n")
-        );
-        return;
-      }
-
-      await sendRecognition(message, userInfo);
-      const gratitudeRemaining = await balance.dailyGratitudeRemaining(
-        userInfo.giver.id,
-        userInfo.giver.tz
-      );
-
-      await Promise.all([
-        sendNotificationToReceivers(bot, message, userInfo),
-        bot.replyEphemeral(
-          message,
-          `Your ${recognizeEmoji} has been sent. You have ${gratitudeRemaining} left to give today.`
-        ),
-      ]);
-    }
+    respondToRecognitionMessage
   );
 
-  controller.on("reaction_added", testFunction);
+  controller.on("reaction_added", respondToRecognitionReaction);
 };
 
-async function testFunction(bot, message) {
+async function respondToRecognitionMessage(bot, message) {
+  winston.info(`Heard reference to ${recognizeEmoji}`, {
+    callingUser: message.user,
+    slackMessage: message.text,
+  });
+
+  let userInfo;
+  try {
+    userInfo = await userDetails(bot, message.text, message.user);
+  } catch (err) {
+    //TODO Error handing
+  }
+
+  const recognitionInfo = {
+    text: message.text,
+    channel: message.channel,
+  };
+
+  await validateAndSendRecognition(bot, message, recognitionInfo, userInfo);
+}
+
+async function respondToRecognitionReaction(bot, message) {
   if (
     !message.reaction.includes(reactionEmoji) ||
     message.item.type !== "message"
@@ -64,22 +53,67 @@ async function testFunction(bot, message) {
     return;
   }
 
-  let originalMessage = await bot.api.conversations.history({
-    channel: message.item.channel,
-    latest: message.item.ts,
-    limit: 1,
-    inclusive: true,
+  winston.info(`Saw a reaction containing ${reactionEmoji}`, {
+    callingUser: message.user,
+    reactionEmoji: message.reaction,
   });
-  originalMessage = originalMessage.messages[0];
-  originalMessage.channel = message.channel;
 
-  const userInfo = await userDetails(bot, originalMessage, message.user);
-  if (!userInfo) {
-    // TODO Error handling
-    return;
+  const messageReactedTo = (
+    await bot.api.conversations.history({
+      channel: message.item.channel,
+      latest: message.item.ts,
+      limit: 1,
+      inclusive: true,
+    })
+  ).messages[0];
+
+  let userInfo;
+  try {
+    userInfo = await userDetails(bot, messageReactedTo.text, message.user);
+  } catch (err) {
+    //TODO Error handing
   }
+  const recognitionInfo = {
+    text: messageReactedTo.text,
+    channel: message.channel,
+  };
 
-  const errors = await checkForRecognitionErrors(originalMessage, userInfo);
+  await validateAndSendRecognition(bot, message, recognitionInfo, userInfo);
+}
+
+async function userDetails(bot, messageText, giver) {
+  const userStrings = messageText.match(userRegex) || [];
+  const userIds = userStrings.map((user) => user.slice(2, -1));
+
+  const userInfo = {
+    giver: await singleUserDetails(bot, giver),
+    receivers: await Promise.all(
+      userIds.map(async (receiver) => singleUserDetails(bot, receiver))
+    ),
+  };
+
+  return userInfo;
+}
+
+async function singleUserDetails(bot, userId) {
+  const singleUserInfo = await bot.api.users.info({ user: userId });
+  if (singleUserInfo.ok) {
+    return singleUserInfo.user;
+  }
+  // TODO Create a better type for this error (Is currently string)
+  throw singleUserInfo.error;
+}
+
+async function validateAndSendRecognition(
+  bot,
+  message,
+  recognitionInfo,
+  userInfo
+) {
+  const errors = await checkForRecognitionErrors(
+    recognitionInfo.text,
+    userInfo
+  );
   if (errors) {
     await bot.replyEphemeral(
       message,
@@ -91,14 +125,15 @@ async function testFunction(bot, message) {
     return;
   }
 
-  await sendRecognition(originalMessage, userInfo);
+  await sendRecognition(recognitionInfo, userInfo);
+
   const gratitudeRemaining = await balance.dailyGratitudeRemaining(
     userInfo.giver.id,
     userInfo.giver.tz
   );
 
-  await Promise.all([
-    sendNotificationToReceivers(bot, originalMessage, userInfo),
+  return Promise.all([
+    sendNotificationToReceivers(bot, recognitionInfo, userInfo),
     bot.replyEphemeral(
       message,
       `Your ${recognizeEmoji} has been sent. You have ${gratitudeRemaining} left to give today.`
@@ -106,49 +141,8 @@ async function testFunction(bot, message) {
   ]);
 }
 
-async function userDetails(bot, message, giver) {
-  const userStrings = message.text.match(userRegex) || [];
-  const userIds = userStrings.map((user) => user.slice(2, -1));
-  const userInfo = {
-    giver: await bot.api.users.info({ user: giver }),
-    receivers: await Promise.all(
-      userIds.map(async (user) => bot.api.users.info({ user: user }))
-    ),
-  };
-
-  return parseUserDetailsRequest(userInfo);
-}
-
-// TODO clean this up. Maybe check for errors after each API call?
-function parseUserDetailsRequest(userInfo) {
-  let infoRequestIsOk = true;
-  /*
-  if (!userInfo.giver.ok) {
-    winston.error("User info request failed", {
-      user: message.user,
-      error: userInfo.giver.error,
-    });
-    infoRequestIsOk = false;
-  }
-  */
-  userInfo.giver = userInfo.giver.user;
-  for (let i = 0; i < userInfo.receivers.length; i++) {
-    /*
-    if (!userInfo.receivers[i].ok) {
-      winston.error("User info request failed", {
-        user: userIds[i],
-        error: userInfo.receivers[i].error,
-      });
-      infoRequestIsOk = false;
-    }
-    */
-    userInfo.receivers[i] = userInfo.receivers[i].user;
-  }
-  return infoRequestIsOk ? userInfo : null;
-}
-
-async function checkForRecognitionErrors(message, userInfo) {
-  const trimmedMessage = message.text
+async function checkForRecognitionErrors(messageText, userInfo) {
+  const trimmedMessage = messageText
     .replace(userRegex, "")
     .replace(generalEmojiRegex, "");
 
@@ -170,7 +164,7 @@ async function checkForRecognitionErrors(message, userInfo) {
     trimmedMessage.length < minimumMessageLength
       ? `- Your message must be at least ${minimumMessageLength} characters`
       : "",
-    !(await isRecognitionWithinSpendingLimits(message, userInfo))
+    !(await isRecognitionWithinSpendingLimits(messageText, userInfo))
       ? `- A maximum of ${maximum} ${recognizeEmoji} can be sent per day`
       : "",
   ]
@@ -178,11 +172,11 @@ async function checkForRecognitionErrors(message, userInfo) {
     .join("\n");
 }
 
-async function isRecognitionWithinSpendingLimits(message, userInfo) {
+async function isRecognitionWithinSpendingLimits(messageText, userInfo) {
   if (usersExemptFromMaximum.includes(userInfo.giver.id)) {
     return true;
   }
-  const emojiInMessage = (message.text.match(recognizeEmojiRegex) || []).length;
+  const emojiInMessage = (messageText.match(recognizeEmojiRegex) || []).length;
   const recognitionGivenToday = await balance.dailyGratitudeRemaining(
     userInfo.giver.id,
     userInfo.giver.tz
@@ -191,9 +185,13 @@ async function isRecognitionWithinSpendingLimits(message, userInfo) {
   return recognitionGivenToday + recognitionInMessage <= maximum;
 }
 
-async function sendRecognition(message, userInfo) {
-  const tags = (message.text.match(tagRegex) || []).map((tag) => tag.slice(1));
-  const emojiCount = (message.text.match(recognizeEmojiRegex) || []).length;
+// TODO Can we add a 'count' field to the recognition?
+async function sendRecognition(recognitionInfo, userInfo) {
+  const tags = (recognitionInfo.text.match(tagRegex) || []).map((tag) =>
+    tag.slice(1)
+  );
+  const emojiCount = (recognitionInfo.text.match(recognizeEmojiRegex) || [])
+    .length;
   let results = [];
   for (let i = 0; i < userInfo.receivers.length; i++) {
     for (let j = 0; j < emojiCount; j++) {
@@ -201,8 +199,8 @@ async function sendRecognition(message, userInfo) {
         recognition.giveRecognition(
           userInfo.giver.id,
           userInfo.receivers[i].id,
-          message.text,
-          message.channel,
+          recognitionInfo.text,
+          recognitionInfo.channel,
           tags
         )
       );
@@ -211,7 +209,7 @@ async function sendRecognition(message, userInfo) {
   return Promise.all(results);
 }
 
-async function sendNotificationToReceivers(bot, message, userInfo) {
+async function sendNotificationToReceivers(bot, recognitionInfo, userInfo) {
   let results = [];
   for (let i = 0; i < userInfo.receivers.length; i++) {
     const numberRecieved = await recognition.countRecognitionsReceived(
@@ -219,7 +217,7 @@ async function sendNotificationToReceivers(bot, message, userInfo) {
     );
     results.push(
       bot.say({
-        text: `You just got recognized by <@${userInfo.giver.id}> in <#${message.channel}> and your new balance is \`${numberRecieved}\`\n>>>${message.text}`,
+        text: `You just got recognized by <@${userInfo.giver.id}> in <#${recognitionInfo.channel}> and your new balance is \`${numberRecieved}\`\n>>>${recognitionInfo.text}`,
         channel: userInfo.receivers[i].id,
       })
     );

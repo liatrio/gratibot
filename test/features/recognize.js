f72c6442edeefc6bb2bf6067e27892fc195bbf32
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-11-22 11:42:23 -0800

    Update recognition unit testing methodology

--- test/features/recognize.js
+++ test/features/recognize.js
@@ -1,34 +1,62 @@
 const sinon = require("sinon");
 const suppressLogs = require("mocha-suppress-logs");
 const expect = require("chai").expect;
-const rewire = require("rewire");
 
 const MockController = require("../mocks/controller");
 
-const recognizeFeature = rewire("../../features/recognize");
+const recognizeFeature = require('../../features/recognize');
+const recognition = require('../../service/recognition');
+const balance = require('../../service/balance');
 
 describe("features/recognize", () => {
+  let controller;
 
-  describe("#checkForRecognitionErrors()", () => {
-    it("should return an empty array when there are no errors", async () => {
-      const messageText = ":fistbump: <@AAA1A1AA1> Test Test Test Test Test";
-      const userInfo = {
-        giver: {
-          id: "BBB2B2BB2",
-          is_bot: false,
-          is_restricted: false,
-        },
-        receivers: [{
-          id: "AAA1A1AA1",
-          is_bot: false,
-          is_restricted:false,
-        }],
-      };
-      const revert = recognizeFeature.__set__("isRecognitionWithinSpendingLimits", () => true);
-      const result = await recognizeFeature.__get__("checkForRecognitionErrors")(messageText, userInfo);
-      revert()
-      expect(result).to.equal("");
+  beforeEach(async () => {
+    controller = new MockController({});
+    await recognizeFeature(controller);
+  })
 
-    });
-  });
+  it("should update database on valid recognition", async () => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    controller.bot.api.users.info = sinon
+      .stub()
+      .onFirstCall()
+      .resolves(
+        {
+          ok: true,
+          user: {
+            id: "AAA1A1AA1",
+            tz: "America/Los_Angeles",
+            is_bot: false,
+            is_restricted: false,
+          },
+        }
+      )
+      .onSecondCall()
+      .resolves(
+        {
+          ok: true,
+          user: {
+            id: "BBB2B2BB2",
+            tz: "America/Los_Angeles",
+            is_bot: false,
+            is_restricted: false,
+          },
+        }
+      )
+
+    const reply = await controller.userInput(
+      {
+        text: ":fistbump: <@BBB2B2BB2> Test Test Test Test Test",
+        user: "AAA1A1AA1",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.true;
+
+  })
 });

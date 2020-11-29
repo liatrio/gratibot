4b747c47ff6e61480b19003b7c41ec7bcea57eb5
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-11-29 10:01:52 -0800

    Restructure testing

--- test/features/recognize.js
+++ test/features/recognize.js
@@ -13,50 +13,206 @@ describe("features/recognize", () => {
 
   beforeEach(async () => {
     controller = new MockController({});
+
+
+    controller.bot.api.users.info
+      .withArgs({ user:"Giver" })
+      .resolves(
+        {
+          ok: true,
+          user: {
+            id: "Giver",
+            tz: "America/Los_Angeles",
+            is_bot: false,
+            is_restricted: false,
+          },
+        }
+      )
+      .withArgs({ user: "Receiver" })
+      .resolves(
+        {
+          ok: true,
+          user: {
+            id: "Receiver",
+            tz: "America/Los_Angeles",
+            is_bot: false,
+            is_restricted: false,
+          },
+        }
+      )
+      .withArgs({ user: "BotUser" })
+      .resolves(
+        {
+          ok: true,
+          user: {
+            id: "BotUser",
+            tz: "America/Los_Angeles",
+            is_bot: true,
+            is_restricted: false,
+          },
+        }
+      )
+      .withArgs({ user: "GuestUser" })
+      .resolves(
+        {
+          ok: true,
+          user: {
+            id: "GuestUser",
+            tz: "America/Los_Angeles",
+            is_bot: false,
+            is_restricted: true,
+          },
+        }
+      );
+
     await recognizeFeature(controller);
   })
+
+  afterEach(() => {
+    sinon.restore();
+  });
 
   it("should update database on valid recognition", async () => {
     const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
     sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
     sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
 
-    controller.bot.api.users.info = sinon
-      .stub()
-      .onFirstCall()
-      .resolves(
-        {
-          ok: true,
-          user: {
-            id: "AAA1A1AA1",
-            tz: "America/Los_Angeles",
-            is_bot: false,
-            is_restricted: false,
-          },
-        }
-      )
-      .onSecondCall()
-      .resolves(
-        {
-          ok: true,
-          user: {
-            id: "BBB2B2BB2",
-            tz: "America/Los_Angeles",
-            is_bot: false,
-            is_restricted: false,
-          },
-        }
-      )
-
-    const reply = await controller.userInput(
+    await controller.userInput(
       {
-        text: ":fistbump: <@BBB2B2BB2> Test Test Test Test Test",
-        user: "AAA1A1AA1",
+        text: ":fistbump: <@Receiver> Test Test Test Test Test",
+        user: "Giver",
         channel: "SomeChannel",
       }
     )
 
     expect(giveRecognition.called).to.be.true;
+  })
 
+  it("shouldn't update database when there are no receivers", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: Test Test Test Test Test",
+        user: "Giver",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when giver matches a receiver", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@Giver> Test Test Test Test Test",
+        user: "Giver",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when giver is a bot user", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@Receiver> Test Test Test Test Test",
+        user: "BotUser",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when giver is a guest user", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@Receiver> Test Test Test Test Test",
+        user: "GuestUser",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when receiver is a bot user", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@BotUser> Test Test Test Test Test",
+        user: "Giver",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when receiver is a guest user", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@GuestUser> Test Test Test Test Test",
+        user: "Giver",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when message is too short", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(5);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@Receiver> Test",
+        user: "Giver",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
+  })
+
+  it("shouldn't update database when giver has no recognition to spend", async() => {
+    const giveRecognition = sinon.stub(recognition, 'giveRecognition').resolves("");
+    sinon.stub(recognition, 'countRecognitionsReceived').resolves(1);
+    sinon.stub(balance, 'dailyGratitudeRemaining').resolves(0);
+
+    await controller.userInput(
+      {
+        text: ":fistbump: <@Receiver> Test",
+        user: "Giver",
+        channel: "SomeChannel",
+      }
+    )
+
+    expect(giveRecognition.called).to.be.false;
   })
 });

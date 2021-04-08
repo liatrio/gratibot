29ee767a54411b4c99cb612eed27c802cb29ca2b
Grant Esparza <esparzagrant@gmail.com>
2021-04-08 14:00:56 -0700

    Add Github Actions workflow

--- tf/variables.tf
+++ tf/variables.tf
@@ -5,22 +5,27 @@ variable acr_subscription_id {
 
 variable resource_group_name {
   description = "Azure resource group for Gratibot"
+  default     = "gratibot-azure-data"
   type        = string
 }
+
 variable location {
   description = "Primary deployment region for Azure resources"
   default     = "Central US"
   type        = string
 }
+
 variable gratibot_image {
   description = "Docker image to be used for Gratibot service"
   type        = string
 }
+
 variable signing_secret {
   description = "Signing secret for Slack app integration"
   type        = string
   sensitive   = true
 }
+
 variable bot_user_token {
   description = "Bot OAuth token for Slack app integration"
   type        = string

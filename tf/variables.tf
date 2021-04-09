2d229f22b9d4bf5d1d866ac549a65ec55de43148
Grant Esparza <grante@liatrio.com>
2021-04-09 10:36:38 -0700

    Update default App Service resource usage (#15)

--- tf/variables.tf
+++ tf/variables.tf
@@ -20,6 +20,25 @@ variable "environment" {
   type        = string
 }
 
+
+variable "instance_tier" {
+  description = "Service plan to use for App Serivce"
+  default     = "Basic"
+  type        = string
+}
+
+variable "instance_size" {
+  description = "Instance size to use for App Serivce"
+  default     = "B1"
+  type        = string
+}
+
+variable "instance_capacity" {
+  description = "Workers associated with App Service Plan"
+  default     = "1"
+  type        = string
+}
+
 variable "gratibot_image" {
   description = "Docker image to be used for Gratibot service"
   type        = string

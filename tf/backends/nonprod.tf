9879e4a50d8a519c952f07a2f0ffbaedad922dd8
Grant Esparza <esparzagrant@gmail.com>
2021-04-13 15:23:15 -0700

    Update to use backend config files

---
+++ tf/backends/nonprod.tf
@@ -0,0 +1,4 @@
+resource_group_name  = "gratibot-azure-data"
+storage_account_name = "gratibotazuredatatfnp"
+container_name       = "tfstate"
+key                  = "terraform.tfstate"
---
+++ tf/backends/prod.tf
@@ -0,0 +1,4 @@
+resource_group_name  = "gratibot-azure-data"
+storage_account_name = "gratibotazuredatatfpd"
+container_name       = "tfstate"
+key                  = "terraform.tfstate"

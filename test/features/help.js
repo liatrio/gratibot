1b8d8c82102c20c4c223c43982c62e13eb0fc131
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2021-05-07 19:21:57 -0700

    Rewrite tests for features/help to avoid use of rewire

--- test/features/help.js
+++ test/features/help.js
@@ -1,57 +1,35 @@
 const sinon = require("sinon");
 const suppressLogs = require("mocha-suppress-logs");
 const expect = require("chai").expect;
-const rewire = require("rewire");
 
 const MockController = require("../mocks/controller");
 
-const helpFeature = rewire("../../features/help");
+const helpFeature = require("../../features/help");
 
-describe("features / help", () => {
-  suppressLogs();
+describe("features/help", () => {
   let controller;
 
-  describe("responds to events", () => {
-    beforeEach(async () => {
-      controller = new MockController({});
-      helpFeature.__set__(
-        "respondToHelp",
-        sinon.spy(helpFeature.__get__("respondToHelp"))
-      );
-      helpFeature.__set__(
-        "respondToEasterEgg",
-        sinon.spy(helpFeature.__get__("respondToEasterEgg"))
-      );
+  beforeEach(async () => {
+    controller = new MockController({});
 
-      await helpFeature(controller);
+    await helpFeature(controller);
+  });
+
+  describe("a help request", () => {
+    it("should respond", async () => {
+      await controller.userInput({ text: "help" });
+      let replies = controller.getReplies();
+
+      expect(replies).to.have.lengthOf(1);
     });
+  });
 
-    context("when user sends help command", function () {
-      let replies;
+  describe("an easter egg request", () => {
+    it("should respond", async () => {
+      await controller.userInput({ text: "thunderfury" });
+      let replies = controller.getReplies();
 
-      beforeEach(async () => {
-        await controller.userInput({ text: "help" });
-        replies = controller.getReplies();
-      });
-
-      it("should respond", async () => {
-        expect(replies).to.have.lengthOf(1);
-        sinon.assert.calledOnce(helpFeature.__get__("respondToHelp"));
-      });
-    });
-
-    context("when user sends thunderfury command", function () {
-      let replies;
-
-      beforeEach(async () => {
-        await controller.userInput({ text: "thunderfury" });
-        replies = controller.getReplies();
-      });
-
-      it("should respond", async () => {
-        expect(replies).to.have.lengthOf(1);
-        sinon.assert.calledOnce(helpFeature.__get__("respondToEasterEgg"));
-      });
+      expect(replies).to.have.lengthOf(1);
     });
   });
 });

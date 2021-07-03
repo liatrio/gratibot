eaa336f541bf8ab321561113024240ff5d35a8c1
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2021-07-03 09:38:48 -0700

    Add basic http respond to respond to Azure pings

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const { App } = require("@slack/bolt");
+const express = require("express");
+
+const webserver = express();
+
+webserver.get("/", (req, res) => {
+  res.send("Gratibot is running!");
+});
+
+webserver.listen(process.env.PORT || 3000);
 
 const app = new App({
   token: process.env.BOT_USER_OAUTH_ACCESS_TOKEN,
@@ -15,7 +24,7 @@ require("fs")
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   console.log("⚡️ Bolt app is running!");
 })();

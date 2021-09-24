60c097657df1c5a32b49d06b502d2944e2c0a2b7
Grant Esparza <grante@liatrio.com>
2021-09-24 08:38:09 -0700

    Increase the fistbump limit to 100 (#144)

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ config.logLevel = process.env.LOG_LEVEL || "info";
 
 config.recognizeEmoji = process.env.RECOGNIZE_EMOJI || ":fistbump:";
 config.reactionEmoji = process.env.REACTION_EMOJI || ":nail_care:";
-config.maximum = 5;
+config.maximum = 100;
 config.minimumMessageLength = 20;
 config.botName = process.env.BOT_NAME || "gratibot";
 

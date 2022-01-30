0288a32e8ce8ae945bbf5fb9ea753fda005bc208
jallen2112 <jallen65@mail.csuchico.edu>
2022-01-30 14:15:30 -0800

    remove duplicate functions

---
+++ service/apiwrappers.js
@@ -0,0 +1,18 @@
+const { SlackError } = require("./errors");
+
+async function userInfo(client, userId) {                                                                                                                                                                                                                                       
+  const response = await client.users.info({ user: userId });    
+  if (response.ok) {    
+    return response.user;    
+  }      
+  throw new SlackError(    
+    "users.info",    
+    response.error,    
+    `Something went wrong while sending recognition. When retreiving user information from Slack, the API responded with the following error: ${response.message} \n Recognition has not been sent.`    
+  );     
+}                                                                                                                                                                      
+
+module.exports = {
+  userInfo,
+}
+

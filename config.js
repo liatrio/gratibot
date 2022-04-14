2c750e0c72c203a81044f392f9ae193392fbd68d
Robert Kelly <RobertKelly@users.noreply.github.com>
2022-04-14 09:16:06 -0500

    chore: adding robert as an exempt user

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ config.botName = process.env.BOT_NAME || "gratibot";
 config.usersExemptFromMaximum = process.env.EXEMPT_USERS?.split(",") || [
   "U037FL37G",
   "U8T585Y8J",
+  "U0K32MUSF",
 ];
 
 config.initialGoldenRecognitionHolder = "UE1QRFSSY";

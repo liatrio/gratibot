cf1c084822a720ed2b3525c3022d543526798f1b
Amber Beasley <amber.beasley@liatrio.com>
2023-05-09 14:44:22 -0400

    update regex for multiplier

--- service/recognition.js
+++ service/recognition.js
@@ -19,7 +19,7 @@ const groupRegex = /<!subteam\^([a-zA-Z0-9]+)\|@([a-zA-Z0-9]+)>/g;
 const tagRegex = /#(\S+)/g;
 const generalEmojiRegex = /:([a-z-_']+):/g;
 const gratitudeEmojiRegex = new RegExp(config.recognizeEmoji, "g");
-const multiplierRegex = /x([0-9]+)/;
+const multiplierRegex = /\:fistbump\:\sx([0-9]+)|x([0-9]+)\s\:fistbump\:/;
 
 // TODO Can we add a 'count' field to the recognition?
 async function giveRecognition(
@@ -183,8 +183,9 @@ async function gratitudeReceiverIdsIn(client, text) {
 
 function gratitudeCountIn(text) {
   const emojiCount = (text.match(gratitudeEmojiRegex) || []).length;
-  const multiplier = text.match(multiplierRegex)
-    ? text.match(multiplierRegex)[1]
+  const multiplierFinding = text.match(multiplierRegex) ? text.match(multiplierRegex).filter(Boolean): null
+  const multiplier = multiplierFinding
+    ? multiplierFinding[1]
     : 1;
   return emojiCount * multiplier;
 }

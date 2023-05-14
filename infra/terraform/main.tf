c079522f422342d655fe7d5e315513ea15ed5520
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2023-05-14 09:54:03 -0700

    fix: specify provider version

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.50.0"
+    }
+  }
+}
+
 provider "azurerm" {
   subscription_id = var.acr_subscription_id
   features {}

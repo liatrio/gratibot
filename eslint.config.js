74e63447220a9e6432e87b3530984f4ddf4ea473
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2024-06-11 08:18:26 -0700

    test: updates eslint to major version 9

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const globals = require("globals");
+const eslint = require("@eslint/js");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const mochaPlugin = require("eslint-plugin-mocha");
+
+module.exports = [
+  eslint.configs.recommended,
+  mochaPlugin.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "mocha/no-mocha-arrows": "off", // Temporary to not include excessive changes in eslint upgrade
+    },
+  },
+];

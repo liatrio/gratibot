0f42bac124ffbd56028b0f0ccfe5e8034a743066
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2024-12-23 12:35:41 -0800

    fix: update release messages

--- .releaserc.js
+++ .releaserc.js
@@ -12,6 +12,27 @@ module.exports = {
       },
     ],
     [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            { type: "feat", section: "Features" },
+            { type: "fix", section: "Bug Fixes" },
+            { type: "perf", section: "Performance Improvements" },
+            { type: "revert", section: "Reverts" },
+            { type: "docs", section: "Documentation" },
+            { type: "style", section: "Styles" },
+            { type: "chore", section: "Miscellaneous Chores" },
+            { type: "refactor", section: "Code Refactoring" },
+            { type: "test", section: "Tests", hidden: true },
+            { type: "build", section: "Build System", hidden: true },
+            { type: "ci", section: "Continuous Integration", hidden: true },
+          ],
+        },
+      },
+    ],
+    [
       "@semantic-release/github",
       {
         successComment: false,

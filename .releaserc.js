1f5558e448eaa192a7aab0c09611fcb2d293e17c
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2024-12-23 12:16:34 -0800

    ci: remove obsolete changelog release automation (#668)

--- .releaserc.js
+++ .releaserc.js
@@ -12,34 +12,6 @@ module.exports = {
       },
     ],
     [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "conventionalcommits",
-        presetConfig: {
-          types: [
-            { type: "feat", section: "Features" },
-            { type: "fix", section: "Bug Fixes" },
-            { type: "perf", section: "Performance Improvements" },
-            { type: "revert", section: "Reverts" },
-            { type: "docs", section: "Documentation" },
-            { type: "style", section: "Styles" },
-            { type: "chore", section: "Miscellaneous Chores" },
-            { type: "refactor", section: "Code Refactoring" },
-            { type: "test", section: "Tests", hidden: true },
-            { type: "build", section: "Build System", hidden: true },
-            { type: "ci", section: "Continuous Integration", hidden: true },
-          ],
-        },
-      },
-    ],
-    [
-      "@semantic-release/changelog",
-      {
-        changelogTitle:
-          "# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.",
-      },
-    ],
-    [
       "@semantic-release/github",
       {
         successComment: false,
